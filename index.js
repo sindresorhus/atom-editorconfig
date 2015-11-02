af1932a57a0712fd92be5f5665f9eb4e7ca806d0
Billy He <qiushihe@me.com>
2015-11-03 03:40:36 +0700

    Close #53 PR: Prevent editor from losing selection due to unnecessary encoding changes.

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ function init(editor) {
 		}
 
 		if (config.charset) {
-			// EditorConfig charset names matches iconv charset names.
-			// Which is used by Atom. So no charset name convertion is needed.
-			editor.setEncoding(config.charset);
+			// by default Atom uses charset name without any dashes in them
+			// (i.e. 'utf16le' instead of 'utf-16le').
+			editor.setEncoding(config.charset.replace(/-/g, '').toLowerCase());
 		}
 	});
 }
