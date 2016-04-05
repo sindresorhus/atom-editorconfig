ed1256dbe43ef0977ce5c1a59113c0aab92f6392
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-04-05 13:33:49 +0200

    add base-spec with minimal fixtures

--- spec/base-spec.js
+++ spec/base-spec.js
@@ -1,2 +1,43 @@
+/** @babel */
+
 /* This file contains all specs to ensure the base-functionality of
-  this plugin. */
+this plugin. */
+
+import _path from 'path';
+// _helpers = require(_path.join(__dirname, '_spec-helpers.coffee'));
+
+const _projectRoot = _path.join(__dirname, 'fixtures');
+
+describe('editorconfig', () => {
+	beforeEach(() => {
+		waitsForPromise(() => {
+			return atom.packages.activatePackage('editorconfig');
+		});
+	});
+
+	describe('when providing base-settings', () => {
+		const filePath = _path.join(_projectRoot, 'base.txt');
+		var texteditor = null;
+
+		atom.workspace.open(filePath)
+    .then((newTexteditor) => {
+			texteditor = newTexteditor;
+		});
+
+		it('should have set the indent_style to "space"', () => {
+			expect(texteditor.getSoftTabs()).toBeTruthy();
+		});
+
+    it('should have set the indent_size to 4 characters', () => {
+      expect(texteditor.getTabLength()).toEqual(4);
+    });
+
+    it('should have set the end_of_line-character to "lf"', () => {
+      expect(texteditor.getBuffer().getPreferredLineEnding()).toMatch("\n");
+    });
+
+    it('should have set the charset of the document to "utf8"', () => {
+      expect(texteditor.getEncoding()).toMatch('utf8');
+    });
+	});
+});
