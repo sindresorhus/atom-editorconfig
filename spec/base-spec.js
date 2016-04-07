4e8f1c9bbc3a6ef5c342c593d724238716cb8cd0
Florian Breisch <florian.breisch@mindkeeper.de>
2016-04-07 09:49:20 +0200

    Introduce base-specs
    * refine travis-config
    * add spec-directory
    * add xo-ignore for atom-dir & apm-specs
    * add base-spec with minimal fixtures
    * add atom-tests to the npm-test-script
    * add build-badge

---
+++ spec/base-spec.js
@@ -0,0 +1,42 @@
+/** @babel */
+
+/* This file contains all specs to ensure the base-functionality of
+this plugin. */
+
+import _path from 'path';
+
+const _projectRoot = _path.join(__dirname, 'fixtures');
+
+describe('editorconfig', () => {
+	beforeEach(() => {
+		waitsForPromise(() => {
+			return atom.packages.activatePackage('editorconfig');
+		});
+	});
+
+	describe('when providing base-settings', () => {
+		const filePath = _path.join(_projectRoot, 'base.txt');
+		let texteditor = null;
+
+		atom.workspace.open(filePath)
+		.then((newTexteditor) => {
+			texteditor = newTexteditor;
+		});
+
+		it('should have set the indent_style to "space"', () => {
+			expect(texteditor.getSoftTabs()).toBeTruthy();
+		});
+
+		it('should have set the indent_size to 4 characters', () => {
+			expect(texteditor.getTabLength()).toEqual(4);
+		});
+
+		it('should have set the end_of_line-character to "lf"', () => {
+			expect(texteditor.getBuffer().getPreferredLineEnding()).toMatch("\n");
+		});
+
+		it('should have set the charset of the document to "utf8"', () => {
+			expect(texteditor.getEncoding()).toMatch('utf8');
+		});
+	});
+});
