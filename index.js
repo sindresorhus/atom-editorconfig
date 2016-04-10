563d437811762ab1eba5d6c5b8556260e0339212
Florian Breisch <florian.breisch@mindkeeper.de>
2016-04-10 11:41:26 +0200

    Fix #79 by improving load- and activation-time
    * enable lazy-loading
    * output load- and activation-time in the test-logs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /** @babel */
-import editorconfig from 'editorconfig';
-import setText from 'atom-set-text';
-import generateConfig from './commands/generate';
+const lazyReq = require('lazy-req')(require);
+
+const editorconfig = lazyReq('editorconfig');
+const setText = lazyReq('atom-set-text');
+const generateConfig = lazyReq('./commands/generate');
 
 function init(editor) {
 	generateConfig();
@@ -21,7 +23,7 @@ function init(editor) {
 		return;
 	}
 
-	editorconfig.parse(file).then(config => {
+	editorconfig().parse(file).then(config => {
 		if (Object.keys(config).length === 0) {
 			return;
 		}
