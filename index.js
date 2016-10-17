4009300909123a035e23919cc85c07f20bfc9bdf
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-10-17 23:35:50 +0200

    Allow fallback-configuration; improve insert_final_newline-implementation
    The introduction of the 'auto'-setting is a preparation for future
    updates, it also makes the whole implementation less intrusive and more
    consistent, since some configuration values now used during the whole
    lifetime of the TextBuffer.
    
    The insert_final_newline-implementation still suffers from intrusive
    actions of the whitespace-plugin. A workaround could be to disable the
    entire whitespace-plugin in favor of atom-editorconfig.

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function observeTextEditor(editor) {
 	if ('editorconfig' in buffer === false) {
 		buffer.editorconfig = {
 			buffer,
-			trimTrailingWhitespaces: false,
-			insertFinalNewline: false,
-			preferredLineEnding: undefined,
-			indentStyle: 'tab',
-			tabLength: '1',
-			encoding: 'utf8',
+			trimTrailingWhitespace: 'auto',
+			insertFinalNewline: 'auto',
+			preferredLineEnding: 'auto',
+			indentStyle: 'auto',
+			tabLength: 'auto',
+			encoding: 'auto',
 
 			// onWillSave-Handler, is currently used to trim whitespaces before buffer is written
 			// to disk
@@ -25,22 +25,42 @@ function observeTextEditor(editor) {
 				const getText = () => {
 					return finalText || this.buffer.getText();
 				};
+				let originalLength = this.buffer.getText().length;
 
-				if (this.trimTrailingWhitespaces) {
+				if (this.trimTrailingWhitespace === true) {
 					finalText = getText().replace(/([ \t]+)$/gm, '');
 				}
 
-				if (this.insertFinalNewline && !getText().endsWith(this.preferredLineEnding)) {
-					finalText = getText().concat(this.preferredLineEnding);
+				if (this.insertFinalNewline !== 'auto') {
+					if (this.insertFinalNewline) {
+						if (getText().endsWith(this.preferredLineEnding) === false) {
+							finalText = getText().concat(this.preferredLineEnding);
+						}
+					} else {
+						while (getText().length > 0 &&
+								getText().charAt(getText().length - 1).search(/\r|\n/) > -1) {
+							finalText = getText().slice(0, -1);
+						}
+					}
 				}
 
 				if (finalText !== undefined) {
 					// Preserve cursor-position of active editor
+					let preservedPosition;
 					const activeTextEditor = atom.workspace.getActiveTextEditor();
-					const currentCursorPosition = activeTextEditor.getCursorBufferPosition();
+					if (activeTextEditor) {
+						preservedPosition = activeTextEditor.getCursorBufferPosition();
+					}
+
+					console.log('Tracked lengths:', originalLength, finalText.length);
 					buffer.setText(finalText);
-					if (activeTextEditor.getBuffer() === buffer) {
-						activeTextEditor.setCursorBufferPosition(currentCursorPosition);
+
+					if (preservedPosition &&
+						activeTextEditor &&
+						activeTextEditor.getBuffer() === buffer) {
+						const row = Math.min(preservedPosition.row, buffer.getLineCount() - 1);
+						const column = Math.min(preservedPosition.column, buffer.lineLengthForRow(row));
+						activeTextEditor.setCursorBufferPosition([row, column]);
 					}
 				}
 			}
@@ -68,32 +88,50 @@ function init(editor) {
 		}
 
 		const bufferConfig = editor.getBuffer().editorconfig;
+		const lineEndings = {
+			crlf: '\r\n',
+			cr: '\r',
+			lf: '\n'
+		};
 
-		// Wisely normalize and initialize config-settings
-		bufferConfig.trimTrailingWhitespaces = config.trim_trailing_whitespaces || false;
-		bufferConfig.insertFinalNewline = config.insert_final_newline || false;
-		bufferConfig.indentStyle = (
-			(config.indent_style.search(/^(space|tab)$/) > -1 &&
-			config.indent_style) ||
-			editor.getSoftTabs() ? 'space' : 'tab'
-		);
-		bufferConfig.preferredLineEnding = (
-			(config.end_of_line.search(/^(\r\n|\n)$/) > -1 &&
-			config.end_of_line) ||
-			editor.getBuffer().getPreferredLineEnding
-		);
-		bufferConfig.tabLength = config.tab_width || config.indent_size || editor.getTabLength();
-		bufferConfig.encoding = (
-			(config.charset &&
-			config.charset.replace(/-/g, '').toLowerCase()) ||
-			editor.getBuffer().getEncoding()
-		);
+		bufferConfig.config = config;
+		// Carefully normalize and initialize config-settings
+		bufferConfig.trimTrailingWhitespace = ('trim_trailing_whitespace' in config) ?
+			config.trim_trailing_whitespace :
+			'auto';
 
-		// Apply settings
-		editor.setSoftTabs(bufferConfig.indentStyle === 'space');
-		editor.setTabLength(bufferConfig.tabLength);
-		editor.getBuffer().setPreferredLineEnding(bufferConfig.preferredLineEnding);
-		editor.getBuffer().setEncoding(bufferConfig.encoding);
+		bufferConfig.insertFinalNewline = ('insert_final_newline' in config) ?
+			config.insert_final_newline :
+			'auto';
+
+		bufferConfig.indentStyle = config.indent_style.search(/^(space|tab)$/) > -1 ?
+			config.indent_style :
+			'auto';
+
+		bufferConfig.preferredLineEnding = lineEndings[config.end_of_line] || 'auto';
+
+		bufferConfig.tabLength = parseInt(config.tab_width || config.indent_size, 10);
+		if (isNaN(bufferConfig.tabLength)) {
+			bufferConfig.tabLength = 'auto';
+		}
+
+		bufferConfig.encoding = ('charset' in config) ?
+			config.charset.replace(/-/g, '').toLowerCase() :
+			'auto';
+
+		// Apply explicit settings
+		if (bufferConfig.indentStyle !== 'auto') {
+			editor.setSoftTabs(bufferConfig.indentStyle === 'space');
+		}
+		if (bufferConfig.tabLength !== 'auto') {
+			editor.setTabLength(bufferConfig.tabLength);
+		}
+		if (bufferConfig.preferredLineEnding !== 'auto') {
+			editor.getBuffer().setPreferredLineEnding(bufferConfig.preferredLineEnding);
+		}
+		if (bufferConfig.encoding !== 'auto') {
+			editor.getBuffer().setEncoding(bufferConfig.encoding);
+		}
 	});
 }
 
