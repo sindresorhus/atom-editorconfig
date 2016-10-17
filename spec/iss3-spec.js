5458a80bddbe1001b48cbe03430475e11c6a00df
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-10-17 14:16:59 +0200

    Implement basic tests for trimming of whitespaces

--- spec/iss3-spec.js
+++ spec/iss3-spec.js
@@ -14,7 +14,8 @@ const filePath = path.join(projectRoot, 'test.iss3');
 
 describe('when saving a file with trailing whitespaces', () => {
 	let textEditor = null;
-	const textWithTrailingWhitespaces = "I am Providence.   "
+	const textWithTrailingWhitespaces = "I am Providence. \t\t  \n";
+	const textWithoutTraillingWhitespaces = "I am Providence.\n";
 
 	beforeEach(() => {
 		waitsForPromise(() =>
@@ -30,23 +31,39 @@ describe('when saving a file with trailing whitespaces', () => {
 	afterEach(() => {
 		// remove the created fixture, if it exists
 		runs(() => {
-			if (fs.statSync(filePath).isFile()) { // TODO: Handle exceptions
-				fs.unlink(filePath);
-			}
+			fs.stat(filePath, (err, stats) => {
+				if (!err && stats.isFile()) {
+					fs.unlink(filePath);
+				}
+			});
 		});
 
 		waitsFor(() => {
-			return fs.statSync(filePath).isFile() == false
-		}, 5000, 'removed ' + filePath);
+			try {
+				return fs.statSync(filePath).isFile() === false;
+			} catch (err) {
+				return true;
+			}
+		}, 5000, `removed ${filePath}`);
 	});
 
 	describe('Atom being set to leave trailing whitespaces', () => {
-		beforeEach(() => {
+		beforeEach(() => textEditor.getBuffer().editorconfig.trimTrailingWhitespaces = false);
 
+		it('should leave the trailing whitespaces.', () => {
+			textEditor.setText(textWithTrailingWhitespaces);
+			textEditor.save();
+			expect(textEditor.getText().length).toEqual(textWithTrailingWhitespaces.length);
 		});
+	});
 
-		it('the file should be left with stripped whitespaces.', () => {
+	describe('Atom being set to strip trailing whitespaces', () => {
+		beforeEach(() => textEditor.getBuffer().editorconfig.trimTrailingWhitespaces = true);
 
+		it('should remove the trailing whitespaces.', () => {
+			textEditor.setText(textWithTrailingWhitespaces);
+			textEditor.save();
+			expect(textEditor.getText().length).toEqual(textWithoutTraillingWhitespaces.length);
 		});
 	});
 });
