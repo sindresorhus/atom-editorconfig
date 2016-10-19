c29af4e6e54941acd70290901b78c61e3fcef20a
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-10-19 18:44:41 +0200

    Implement notifications, statusbar-icon and failure-awareness

--- index.js
+++ index.js
@@ -3,70 +3,129 @@ import generateConfig from './commands/generate';
 
 const lazyReq = require('lazy-req')(require);
 
-const StatusTile = lazyReq('./lib/statustile-view');
+const statusTile = lazyReq('./lib/statustile-view');
 const editorconfig = lazyReq('editorconfig');
 
-const statusTile = StatusTile().create({status: 'subtle'});
+const STATES = ['subtle', 'success', 'info', 'warning', 'error'];
+const BLACKLISTED_PACKAGES = {
+	whitespace: ['insert_final_newline', 'trim_trailing_whitespace']
+};
+
+function setState(ecfg) {
+	const messages = [];
+	let statcon = 0;
+
+	if (Object.keys(ecfg.settings).reduce((prev, curr) => {
+		return ecfg.settings[curr] !== 'auto' || prev;
+	}, false)) {
+		statcon = Math.max(statcon, 1);
+	}
+
+	// Check the 'Tab Type'-setting
+	if (ecfg.settings.indent_style !== 'auto' &&
+		atom.config.get('editor.tabType') !== 'auto') {
+		const tabType = atom.config.get('editor.tabType');
+
+		messages.push(`**Tab Type:** You editor's configuration setting "Tab Type"
+		(currently "${tabType}" prevents the editorconfig-property \`indent_style\` from working.
+		@"Tab Type" **must** be set to "auto" to fix this issue.`);
+
+		statcon = Math.max(statcon, 4);
+	}
+
+	// Check for BLACKLISTED packages
+	const suspiciuousPackages = {};
+	let affectedProperties;
+	for (const packageName in BLACKLISTED_PACKAGES) {
+		if ({}.hasOwnProperty.call(BLACKLISTED_PACKAGES, packageName)) {
+			affectedProperties = BLACKLISTED_PACKAGES[packageName].filter(prop => {
+				return ecfg.settings[prop] !== 'auto';
+			});
+			if (affectedProperties.length > 0 &&
+				atom.packages.isPackageActive(packageName)) {
+				suspiciuousPackages[packageName] = affectedProperties;
+			}
+		}
+	}
+	if (Object.keys(suspiciuousPackages).length > 0) {
+		for (const packageName in suspiciuousPackages) {
+			if ({}.hasOwnProperty.call(suspiciuousPackages, packageName)) {
+				const properties = suspiciuousPackages[packageName];
+				messages.push(`**${packageName}:** It is likely that the
+				${packageName}-package prevents the following
+				propert${properties.length > 1 ? 'ies' : 'y'} from working reliably:
+				\`${properties.join('`, `')}\`.@You may deactivate or disable the ${packageName}-package
+				to fix that issue.`);
+			}
+		}
+		statcon = Math.max(statcon, 3);
+	}
+
+	ecfg.messages = messages;
+	ecfg.state = STATES[statcon];
+	statusTile().update(ecfg.state);
+}
 
 function observeActivePaneItem(editor) {
-	if (editor &&
-		editor.constructor &&
-		editor.constructor.name == 'TextEditor') {
+	if (editor && editor.constructor.name === 'TextEditor') {
 		if (editor.getBuffer().editorconfig) {
 			editor.getBuffer().editorconfig.applySettings();
 		}
 	}
 }
 
-function initializeTextEditor(editor) {
-	const buffer = editor.getBuffer();
+function initializeTextBuffer(buffer) {
 	if ('editorconfig' in buffer === false) {
 		buffer.editorconfig = {
-			buffer,
+			buffer, // preserving a reference to the parent TextBuffer
 			state: 'subtle',
-			trimTrailingWhitespace: 'auto',
-			insertFinalNewline: 'auto',
-			preferredLineEnding: 'auto',
-			indentStyle: 'auto',
-			tabLength: 'auto',
-			encoding: 'auto',
+			settings: {
+				trim_trailing_whitespace: 'auto', // eslint-disable-line camelcase
+				insert_final_newline: 'auto', // eslint-disable-line camelcase
+				end_of_line: 'auto', // eslint-disable-line camelcase
+				indent_style: 'auto', // eslint-disable-line camelcase
+				tab_width: 'auto', // eslint-disable-line camelcase
+				charset: 'auto' // eslint-disable-line camelcase
+			},
 
 			applySettings() {
 				const editor = atom.workspace.getActiveTextEditor();
+				const settings = this.settings;
 
-				if (editor && editor.getBuffer() === this.buffer) {
-					if (this.indentStyle !== 'auto') {
-						editor.setSoftTabs(this.indentStyle === 'space');
+				if (editor && editor.getBuffer() === buffer) {
+					if (settings.indent_style !== 'auto') {
+						editor.setSoftTabs(settings.indent_style === 'space');
 					}
-					if (this.tabLength !== 'auto') {
-						editor.setTabLength(this.tabLength);
+					if (settings.tab_width !== 'auto') {
+						editor.setTabLength(settings.tab_width);
 					}
-					if (this.preferredLineEnding !== 'auto') {
-						this.buffer.setPreferredLineEnding(this.preferredLineEnding);
+					if (settings.end_of_line !== 'auto') {
+						buffer.setPreferredLineEnding(settings.end_of_line);
 					}
-					if (this.encoding !== 'auto') {
-						this.buffer.setEncoding(this.encoding);
+					if (settings.charset !== 'auto') {
+						buffer.setEncoding(settings.charset);
 					}
 				}
+				setState(this);
 			},
-			// onWillSave-Handler, is currently used to trim whitespaces before buffer is written
-			// to disk
-			onWillSave() {
-				let finalText;
-				// Fetch the text-buffer lazily
-				const getText = () => {
-					return finalText || this.buffer.getText();
-				};
-				let originalLength = this.buffer.getText().length;
 
-				if (this.trimTrailingWhitespace === true) {
+			// onWillSave-Handler
+			// trims whitespaces and inserts/strips final newline before saving
+			onWillSave() {
+				const settings = this.settings;
+				let finalText;
+				const getText = () => {
+					return finalText || buffer.getText();
+				};
+
+				if (settings.trim_trailing_whitespace === true) {
 					finalText = getText().replace(/([ \t]+)$/gm, '');
 				}
 
-				if (this.insertFinalNewline !== 'auto') {
-					if (this.insertFinalNewline) {
-						if (getText().endsWith(this.preferredLineEnding) === false) {
-							finalText = getText().concat(this.preferredLineEnding);
+				if (settings.insert_final_newline !== 'auto') {
+					if (settings.insert_final_newline) {
+						if (getText().endsWith(settings.end_of_line) === false) {
+							finalText = getText().concat(settings.end_of_line);
 						}
 					} else {
 						while (getText().length > 0 &&
@@ -105,10 +164,9 @@ function observeTextEditors(editor) {
 	if (!editor) {
 		return;
 	}
-	initializeTextEditor(editor);
+	initializeTextBuffer(editor.getBuffer());
 
 	const file = editor.getURI();
-
 	if (!file) {
 		return;
 	}
@@ -118,40 +176,48 @@ function observeTextEditors(editor) {
 			return;
 		}
 
-		const bufferConfig = editor.getBuffer().editorconfig;
+		const ecfg = editor.getBuffer().editorconfig;
+		const settings = ecfg.settings;
 		const lineEndings = {
 			crlf: '\r\n',
 			cr: '\r',
 			lf: '\n'
 		};
 
-		bufferConfig.config = config;
+		// Preserve evaluated Editorconfig
+		ecfg.config = config;
+
 		// Carefully normalize and initialize config-settings
-		bufferConfig.trimTrailingWhitespace = ('trim_trailing_whitespace' in config) ?
+		// eslint-disable-next-line camelcase
+		settings.trim_trailing_whitespace = ('trim_trailing_whitespace' in config) ?
 			config.trim_trailing_whitespace :
 			'auto';
 
-		bufferConfig.insertFinalNewline = ('insert_final_newline' in config) ?
+		// eslint-disable-next-line camelcase
+		settings.insert_final_newline = ('insert_final_newline' in config) ?
 			config.insert_final_newline :
 			'auto';
 
-		bufferConfig.indentStyle = config.indent_style.search(/^(space|tab)$/) > -1 ?
+		// eslint-disable-next-line camelcase
+		settings.indent_style = config.indent_style.search(/^(space|tab)$/) > -1 ?
 			config.indent_style :
 			'auto';
 
-		bufferConfig.preferredLineEnding = lineEndings[config.end_of_line] || 'auto';
+		// eslint-disable-next-line camelcase
+		settings.end_of_line = lineEndings[config.end_of_line] || 'auto';
 
-		bufferConfig.tabLength = parseInt(config.tab_width || config.indent_size, 10);
-		if (isNaN(bufferConfig.tabLength)) {
-			bufferConfig.tabLength = 'auto';
+		// eslint-disable-next-line camelcase
+		settings.tab_width = parseInt(config.tab_width || config.indent_size, 10);
+		if (isNaN(settings.tab_width)) {
+			settings.tab_width = 'auto'; // eslint-disable-line camelcase
 		}
 
-		bufferConfig.encoding = ('charset' in config) ?
+		settings.charset = ('charset' in config) ?
 			config.charset.replace(/-/g, '').toLowerCase() :
 			'auto';
 
 		// Apply initially
-		bufferConfig.applySettings();
+		ecfg.applySettings();
 	});
 }
 
@@ -159,13 +225,13 @@ const activate = () => {
 	generateConfig();
 	atom.workspace.observeTextEditors(observeTextEditors);
 	atom.workspace.observeActivePaneItem(observeActivePaneItem);
-}
+};
 
 const consumeStatusBar = statusBar => {
 	statusBar.addRightTile({
-		item: statusTile,
+		item: statusTile().create(),
 		priority: 999
 	});
-}
+};
 
 export default {activate, consumeStatusBar};
